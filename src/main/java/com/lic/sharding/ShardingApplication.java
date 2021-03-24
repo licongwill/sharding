b22a218097398b3d6d9cc25874eca286bb7d946c
licong <licongcong.lcc@sunyur.com>
2021-03-24 22:44:43 +0800

    集成logback

--- src/main/java/com/lic/sharding/ShardingApplication.java
+++ src/main/java/com/lic/sharding/ShardingApplication.java
@@ -1,8 +1,12 @@
 package com.lic.sharding;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import com.lic.sharding.dao.UserMapper;
 import com.lic.sharding.domain.User;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
